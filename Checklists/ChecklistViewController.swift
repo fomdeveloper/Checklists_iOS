06a26032bd722415ee0e081be5073e06b6f6b677
FOMDeveloper <fomdeveloper@gmail.com>
2014-11-03 17:27:46 +0000

    Add and edit new checklists

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -128,6 +128,9 @@ class ChecklistViewController: UITableViewController,ItemDetailViewControllerDel
     }
     
     
+
+    
+    
     func configureTextForCell(cell:UITableViewCell , withChecklistItem item: ChecklistItem){
         let label = cell.viewWithTag(1000) as UILabel;
         label.text = item.text;
